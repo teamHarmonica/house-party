c1b1205ca44723bb4b2bdf2514869de1f4ba3900
farahzuot <farahz997@eng.hu.edu.jo>
2021-02-07 09:19:51 +0200

    error style and allowed host

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 import { TextField, Button, Grid, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+const myTheme =  createMuiTheme({
+    palette: {
+        primary: {
+          main: '#2402ba',
+        },
+        secondary: {
+          main: '#6200EE',
+        },
+        error: {
+          main: '#ffffff',
+        },
+    },
+});
 
 export default class RoomJoinPage extends Component {
   constructor(props) {
@@ -21,17 +37,19 @@ export default class RoomJoinPage extends Component {
             border: '2px solid #0d0e4a', padding:'70px', borderRadius:"7px"
             }}>
         
-            <h1 style={{ color: "#0d0e4a", fontFamily: "Musicografi", fontSize:70, marginBottom:"50px" }}> Join Room</h1>               
-            <TextField
-              style={{ marginBottom: "75px", width:"320px"}}
-              error={this.state.error}
-              label="Code"
-              placeholder="Enter a Room Code"
-              value={this.state.roomCode}
-              helperText={this.state.error}
-              variant="outlined"
-              onChange={this.handleTextFieldChange}
-            />
+            <h1 style={{ color: "#0d0e4a", fontFamily: "Musicografi", fontSize:70, marginBottom:"50px" }}> Join Room</h1>
+            <MuiThemeProvider theme = {myTheme}>
+              <TextField
+                style={{ marginBottom: "75px", width:"320px"}}
+                error={this.state.error}
+                label="Code"
+                placeholder="Enter a Room Code"
+                value={this.state.roomCode}
+                helperText={this.state.error}
+                variant="outlined"
+                onChange={this.handleTextFieldChange}
+              />
+            </MuiThemeProvider>
             <Grid item xs={12} align="center">
               <Button
                 variant="contained"
